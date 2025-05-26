1e253fd25631909ab8b8ecc81431fa3bb442ed4e
Francesco <frenk0004@gmail.com>
2025-05-26 12:42:13 +0200

    aggiunto layout della pagina login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 gap-4 p-4">
-      <div className="flex flex-col gap-4 bg-gray-100 p-6 text-center">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <p className="text-gray-600">This is a simple Next.js app.</p>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col gap-4 bg-gray-100  p-8 w-full max-w-md authCustom">
+        <h1 className="text-2xl font-semibold pt-6">Login</h1>
+        <p className="text-sm textCustom">Don&apos;t you have an account? <a className="text-black font-bold" href="">Signup</a></p>
+        <form>
+          <div className="mb-4">
+            <input className="inputCustom w-full focus:outline-none focus:ring-2 focus:ring-black/10 focus:border-black transition duration-200" type="email" placeholder="Email" />
+          </div>
+          <div className="mb-4">
+            <input className="inputCustom w-full focus:outline-none focus:ring-2 focus:ring-black/10 focus:border-black transition duration-200" type="password" placeholder="Password" />
+          </div>
+          <button className="btnCustom w-full p-2 mt-4 text-white" type="submit">Continue</button>
+        </form>
+        <a className="text-black font-medium mt-3 pb-3 text-center" href="">Forgot Password?</a>
       </div>
     </div>
   )
